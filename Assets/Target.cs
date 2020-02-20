22d23b74d480b87fcd0cc5fa62a034792dd10556
ccal9 <36898708+ccal9@users.noreply.github.com>
2020-02-20 01:55:16 -0500

    updated prefab with rb and started script

---
+++ Assets/Target.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    [Header("Set in Inspector")]
+    public GameObject targetPrefab;
+    public int maxTargets = 7;
+    public int maxScale = 3;
+    public float frequency ;
+    public float speed;
+
+    private List<GameObject> Targets;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Targets = new List<GameObject>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
